6296e2d2f1b7e80184264ec09d4a74c241bd7c16
Alex Martini <104405125+konatown@users.noreply.github.com>
2025-05-30 14:07:23 -0400

    Add files via upload

---
+++ GetEmailMessageHeadersandBody.ps1
@@ -0,0 +1,31 @@
+﻿Write-host -ForegroundColor darkgray 'Script Written By Alex Martini
+'
+Write-host -ForegroundColor darkgray 'You must have the MS Graph SDK Powershell Module installed to run this script
+'
+
+Write-host -ForegroundColor darkgray "This script will retrieve the message headers for an E-Mail
+You will need the Online Message ID from the More Information section of a Message Trace
+"
+#Connect to MS Graph
+Connect-MgGraph 
+
+#Gather parameters
+$UPN = Read-Host -Prompt '
+Input the E-Mail address of the recipient.
+>'
+
+$MessageID = Read-Host -Prompt '
+Input the full internet message ID from the Message Trace. Ex. "<BN0PR13MB5149C3E4SLEK2355JENSQ@BN0PR13MB5149.namprd13.prod.outlook.com>" You must include the angle brackets.
+>'
+
+
+
+$Message = Get-MgUserMessage -UserId $UPN -Filter "internetMessageId eq '$MessageID'"
+
+Write-host Subject: $Message.Subject
+Write-host Body: ($Message.Body |fl |out-string)
+Write-host Headers: ($Message.InternetMessageHeaders|fl |out-string)
+
+Read-Host -Prompt 'Script finished, Press Enter to exit'
+
+Disconnect-MgGraph
